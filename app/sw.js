c2aa9f733e07d1e3357a0626c88b509ff0209102
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-06-15 11:04:33 -0400

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,11 +1,11 @@
 // https://web.dev/learn/pwa/serving/#stale-while-revalidate
-addEventListener("fetch", e => {
+addEventListener('fetch', e => {
     e.respondWith(
         caches.match(e.request).then(cachedResponse => {
-            const networkFetch = fetch(event.request).then(response => {
+            const networkFetch = fetch(e.request).then(response => {
                 // update the cache with a clone of the network response (if it didn't 404 or not connected)
                 if (response.ok)
-                    caches.open("pwa-assets").then(cache => {
+                    caches.open('pwa-assets').then(cache => {
                         cache.put(e.request, response.clone());
                     });
                 return response;
@@ -17,12 +17,26 @@ addEventListener("fetch", e => {
     )
 });
 
-addEventListener("install", e => {
-    console.log("Service worker installed");
+addEventListener('install', e => {
+    console.log('Service worker installed');
     skipWaiting();
 });
 
-addEventListener("activate", e => {
-    console.log("Service worker activated");
+addEventListener('activate', e => {
+    console.log('Service worker activated');
     e.waitUntil(clients.claim());
+});
+
+addEventListener('notificationclick', e => {
+    console.log('Notification clicked:', e.notification.id, e.action);
+    e.waitUntil(async () => {
+        const allClients = await clients.matchAll({ type: 'window' });
+        for (var client of allClients) {
+            client.postMessage({
+                event: 'notificationclick',
+                notification: e.notification,
+                action: e.action,
+            });
+        }
+    });
 });
